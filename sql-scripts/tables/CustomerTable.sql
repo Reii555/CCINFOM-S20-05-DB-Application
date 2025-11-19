bcad982f3103339d40e72bb2eea42d1d4eacaf46
lumvon <vaughn_lumbang@dlsu.edu.ph>
2025-11-20 00:12:03 +0800

    Update CustomerTable.sql

--- sql-scripts/tables/CustomerTable.sql
+++ sql-scripts/tables/CustomerTable.sql
@@ -1,6 +1,6 @@
 USE pickups;
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id DECIMAL(10,0) PRIMARY KEY,
     customer_lastname VARCHAR(20) NOT NULL,
     customer_firstname VARCHAR(20) NOT NULL,

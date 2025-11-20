13a0190c513c982cec400380b3999cb27922f37e
Reii555 <SRLagoos@gmail.com>
2025-11-20 23:54:54 +0800

    Update DatabaseConnection.java

--- java-app/DatabaseConnection.java
+++ java-app/DatabaseConnection.java
@@ -1,10 +1,12 @@
 import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
 
 public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/delivery_pickup_db"; //replace whoever will make da db server
+        String url = "jdbc:mysql://localhost:3306/delivery_pickup_db??useSSL=false&allowPublicKeyRetrieval=true"; //replace whoever will make da db server
         String username = "root";       //replace whoever will make da db server
-        String password = "password";   //replace whoever will make da db server
+        String password = "haisqluserhehehehe11)";   //replace with ur workbench pass
 
         return DriverManager.getConnection(url, username, password);
     }

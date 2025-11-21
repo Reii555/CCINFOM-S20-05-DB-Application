7d0ae9e81bbd0d89dacf8eca895a9d03c529eb85
Reii555 <SRLagoos@gmail.com>
2025-11-21 20:09:10 +0800

    Add files via upload

---
+++ REALEST FILE/BACKENDS/DeliveryLocationDensityReportModel.java
@@ -0,0 +1,33 @@
+public class DeliveryLocationDensityReportModel {
+    private String location;
+    private int totalCompleteDeliveries;
+    private String month;
+    private int year;
+    private int rank;
+
+    public DeliveryLocationDensityReportModel(String location, int totalCompleteDeliveries, String month, int year, int rank) {
+        this.location = location;
+        this.totalCompleteDeliveries = totalCompleteDeliveries;
+        this.month = month;
+        this.year = year;
+        this.rank = rank;
+}
+
+    // getterz
+    public String getLocation() {
+        return location;
+    }
+    public int getTotalCompleteDeliveries() {
+        return totalCompleteDeliveries;
+    }
+    public String getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getRank() {
+        return rank;
+    }
+
+}
